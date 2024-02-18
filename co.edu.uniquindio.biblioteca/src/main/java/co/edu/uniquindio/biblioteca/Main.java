e4d8353107b4703a78d1ddfaf5beb5e0e863f9fd
Andres1452 <141796724+Andres1452@users.noreply.github.com>
2024-02-17 19:26:06 -0500

    creacion de clase pokemon

--- co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/Main.java
+++ co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/Main.java
@@ -2,6 +2,7 @@ package co.edu.uniquindio.biblioteca;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
     }
 }
---
+++ co.edu.uniquindio.biblioteca/src/main/java/co/edu/uniquindio/biblioteca/Pokemon.java
@@ -0,0 +1,17 @@
+package co.edu.uniquindio.biblioteca;
+
+public abstract class Pokemon {
+
+    protected int numPokedex;
+    protected String NombrePokemon;
+    protected double Peso;
+    protected String Sexo;
+    protected int Temporada;
+
+    protected abstract void  atacarPlacaje ();
+    protected abstract void  atacarGarraso ();
+    protected abstract void  atacarMordisco ();
+
+    
+
+}
